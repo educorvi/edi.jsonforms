134c7abc1ae2ac6f38ee938e937a0b8dfe640556
Nina <nina@ninas-vm.ninas-vm>
2024-05-14 11:56:05 +0200

    extension of array and complex

--- src/edi/jsonforms/content/array.py
+++ src/edi/jsonforms/content/array.py
@@ -13,48 +13,10 @@ from zope.interface import implementer
 # from edi.jsonforms import _
 
 
-class IArray(model.Schema):
+class IArray(IComplex):
     """ Marker interface and Dexterity Python Schema for Array
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('array.xml')
-
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
-
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
 
 
 @implementer(IArray)
