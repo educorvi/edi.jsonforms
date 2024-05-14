a4837935ea682ca7b8dea18e764bf6d349b21391
Nina <nina@ninas-vm.ninas-vm>
2024-05-14 12:12:14 +0200

    added attribute for option

--- src/edi/jsonforms/content/option.py
+++ src/edi/jsonforms/content/option.py
@@ -8,53 +8,26 @@ from plone.supermodel import model
 # from z3c.form.browser.radio import RadioFieldWidget
 # from zope import schema
 from zope.interface import implementer
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 
-# from edi.jsonforms import _
+from edi.jsonforms import _
 
+
+field_types = [
+    SimpleTerm('text', 'text', _('Textline')),
+    SimpleTerm('textarea', 'textarea', _('Textarea')),
+    SimpleTerm('number', 'number', _('Number'))
+]
+Field_types = SimpleVocabulary(field_types)
 
 class IOption(model.Schema):
     """ Marker interface and Dexterity Python Schema for Option
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
+    title = schema.TextLine(title=_('Label of the answer option'))
 
-    # model.load('option.xml')
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
-
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
 
 
 @implementer(IOption)
