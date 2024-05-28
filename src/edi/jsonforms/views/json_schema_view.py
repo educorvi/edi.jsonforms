dc025db0a3826b2c4b3df00d75e2106cc3dfe606
Nina Mücke <nina.muecke@educorvi.de>
2024-05-28 12:59:00 +0200

    first working version json schema view including requiredDependent objects

--- src/edi/jsonforms/views/json_schema_view.py
+++ src/edi/jsonforms/views/json_schema_view.py
@@ -13,8 +13,9 @@ class JsonSchemaView(BrowserView):
     # template = ViewPageTemplateFile('json_schema_view.pt')
     jsonschema = {}
 
+    possibly_required_types = ['Field', 'SelectionField', 'Array']
+
     def __call__(self):
-        import pdb; pdb.set_trace()
         form = self.context
         self.jsonschema['type'] = 'object'
         self.jsonschema = add_title_and_description(self.jsonschema, form.title, form.description)
@@ -22,34 +23,66 @@ class JsonSchemaView(BrowserView):
         children = form.getFolderContents()
         self.jsonschema['properties'] = {}
         self.jsonschema['required'] = []
+        self.jsonschema['dependentRequired'] = {}
         for child in children:
             child_object = child.getObject()
+            child_id = create_id(child.id, child_object.UID())
 
             # add children to the schema
             if child_object.portal_type == 'Fieldset':
                 self.jsonschema = self.modify_schema_for_fieldset(self.jsonschema, child_object)
             else:
-                self.jsonschema['properties'][child.id + child_object.UID()] = self.get_schema_for_child(child_object)
+                self.jsonschema['properties'][child_id] = self.get_schema_for_child(child_object)
 
             # mark children as required
-            if child_object.portal_type in ['Field', 'SelectionField', 'Array'] and child_object.required_choice == 'required':
-                self.jsonschema['required'].append(child.id + child_object.UID())
+            if child_object.portal_type in self.possibly_required_types and child_object.required_choice == 'required':
+                if child_object.dependent_from_object is not None:
+                    self.jsonschema = self.add_dependent_required(self.jsonschema, child_object, child_id)
+                else:
+                    self.jsonschema['required'].append(child_id)
 
         # Implement your own actions:
         self.msg = json.dumps(self.jsonschema)
         return self.index()
 
+    def add_dependent_required(self, schema, child_object, child_id):
+        dependent_from = child_object.dependent_from_object.to_object
+        dependent_id = create_id(dependent_from.id, dependent_from.UID())
+        if dependent_from.portal_type == 'Option':
+            selection_parent = dependent_from.aq_parent
+            if_then = {
+                'if': {
+                    'properties': {
+                        create_id(selection_parent.id, selection_parent.UID()): {'const': dependent_from.title}
+                    }
+                },
+                'then': {
+                    'required': child_id
+                }
+            }
+            if 'allOf' in schema:
+                schema['allOf'].append(if_then)
+            else:
+                schema['allOf'] = [if_then]
+        else:
+            if dependent_id in schema['dependentRequired']:
+                schema['dependentRequired'][dependent_id].append(child_id)
+            else:
+                schema['dependentRequired'][dependent_id] = [child_id]
+        return schema
+
     def modify_schema_for_fieldset(self, schema, fieldset):
-        import pdb;pdb.set_trace()
         children = fieldset.getFolderContents()
         for child in children:
             child_object = child.getObject()
+            child_id = create_id(child.id, child_object.UID())
             if child_object.portal_type == 'Fieldset':
                 schema = modify_schema_for_fieldset(self, schema, child_object)
             else:
-                schema['properties'][child.id + child_object.UID()] = self.get_schema_for_child(child_object)
-                if child_object.portal_type in ['Field', 'SelectionField', 'Array'] and child_object.required_choice == 'required':
-                    schema['required'][child.id + child_object.UID()].append(child.title)
+                schema['properties'][child_id] = self.get_schema_for_child(child_object)
+                if child_object.portal_type in self.possibly_required_types and child_object.required_choice == 'required':
+                    schema['required'].append(child_id)
+                    schema = add_title_and_description(schema, child_object.title, child_object.description)
         return schema
 
     def get_schema_for_child(self, child):
@@ -108,13 +141,9 @@ class JsonSchemaView(BrowserView):
         if answer_type == 'radio' or answer_type == 'select':
             selectionfield_schema['type'] = 'string'
             selectionfield_schema['enum'] = []
-            for o in options:
-                selectionfield_schema['enum'].append(o.Title)
         elif answer_type == 'checkbox' or answer_type == 'selectmultiple':
             selectionfield_schema['type'] = 'array'
             selectionfield_schema['enum'] = []
-            for o in options:
-                selectionfield_schema['enum'].append(o.Title)
         elif answer_type == 'file':
             selectionfield_schema['type'] = 'string'
             selectionfield_schema['format'] = 'uri'
@@ -124,6 +153,10 @@ class JsonSchemaView(BrowserView):
                 'type': 'string',
                 'format': 'uri'
             }
+
+        if answer_type in ['radio', 'checkbox']:
+            for o in options:
+                selectionfield_schema['enum'].append(o.Title)
         return selectionfield_schema
 
     def get_schema_for_array(self, array):
@@ -144,18 +177,22 @@ class JsonSchemaView(BrowserView):
         complex_schema['required'] = []
 
         children = object.getFolderContents()
-        for c in children:
-            c_obj = c.getObject()
+        for child in children:
+            child_object = child.getObject()
+            child_id = create_id(child.id, child_object.UID())
 
             # add children to the schema
-            if c_obj.portal_type == 'Fieldset':
-                complex_schema = self.modify_schema_for_fieldset(complex_schema, c_obj)
+            if child_object.portal_type == 'Fieldset':
+                complex_schema = self.modify_schema_for_fieldset(complex_schema, child_object)
             else:
-                complex_schema['properties'][c.id + c_obj.UID()] = self.get_schema_for_child(c_obj)
+                complex_schema['properties'][create_id(child.id, child_object.UID())] = self.get_schema_for_child(child_object)
 
             # mark children as required
-            if c_obj.portal_type in ['Field', 'SelectionField', 'Array'] and c_obj.required_choice == 'required':
-                complex_schema['required'].append(c.id + c_obj.UID())
+            if child_object.portal_type in self.possibly_required_types and child_object.required_choice == 'required':
+                if child_object.dependent_from_object is not None:
+                    complex_schema = self.add_dependent_required(complex_schema, child_object, child_id)
+                else:
+                    complex_schema['required'].append(child_id)
 
         return complex_schema
 
@@ -166,3 +203,6 @@ def add_title_and_description(schema, title, description):
     if description:
         schema['description'] = description
     return schema
+
+def create_id(id, uid):
+    return str(id) + str(uid)
