02a37da7589b213d7c155dbc59a2f1147bb59923
Lars Walther <lwalther@novareto.de>
2025-10-20 17:37:49 +0200

    Developer Area eingeführt

--- src/edi/jsonforms/views/form_view.py
+++ src/edi/jsonforms/views/form_view.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
-
 from edi.jsonforms import _
 from Products.Five.browser import BrowserView
 from typing import Callable
 from plone import api
 from plone.app.layout.viewlets.content import ContentHistoryView
-#from Products.CMFPlone.resources import add_resource_on_request
-
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
 
 class FormView(BrowserView):
 
     def __call__(self):
+        alsoProvides(self.request, IDisableCSRFProtection)
         return self.index()
 
     def get_versions(self):
+        repo_tool = api.portal.get_tool(name='portal_repository')
         historylist = []
         fullhistory = ContentHistoryView(self.context, self.request).fullHistory()
         if not fullhistory:
             return historylist
         for release in fullhistory:
-            import pdb;pdb.set_trace()
             if release.get('version_id'):
-                versionmarker = release.get('version_id')
-                url = self.context.absolute_url() + f'/@@schema-view?version={versionmarker}'
-                deptree_url = self.context.absolute_url() + f'/@@deptree-view?version={versionmarker}'
-                deform_url = self.context.absolute_url() + f'/@@deform-view?version={versionmarker}'
-                historylist.append((release.get('version_id'), release.get('comments'), url, deptree_url, deform_url))
+                version = release.get('version_id')
+                obj = repo_tool.retrieve(self.context, int(version)).object
+                schema_url = self.context.absolute_url() + f'/@@version-view?version={version}&schema=json'
+                ui_url = self.context.absolute_url() + f'/@@version-view?version={version}&schema=ui'
+                historylist.append((release.get('version_id'), release.get('comments'), schema_url, ui_url))
         return historylist
-
--- src/edi/jsonforms/views/version_view.py
+++ src/edi/jsonforms/views/version_view.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import json
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
+from plone import api
 
 class IVersionView(Interface):
     """ Marker Interface for IVersionView"""
@@ -11,7 +12,14 @@ class IVersionView(Interface):
 @implementer(IVersionView)
 class VersionView(BrowserView):
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+        repo_tool = api.portal.get_tool(name='portal_repository')
+        if self.request.get('version') and self.request.get('schema'):
+            version = self.request.get('version')
+            schema = self.request.get('schema')
+            obj = repo_tool.retrieve(self.context, int(version)).object
+            if schema == 'json':
+                objschema = obj.json_schema_rev
+            elif schema == 'ui':
+                objschema = obj.ui_schema_rev
+            return json.dumps(objschema, indent=4)
+        return self.request.response.redirect(self.context.absolute_url())
