0e742ba71ae216325772b6bebf4947188070513b
Nina Mücke <nina.muecke@educorvi.de>
2025-11-11 13:17:58 +0100

    refactor: format

--- src/edi/jsonforms/views/json_schema_view.py
+++ src/edi/jsonforms/views/json_schema_view.py
@@ -11,8 +11,8 @@ from edi.jsonforms.content.option_list import get_keys_and_values_for_options_li
 
 
 class JsonSchemaView(BrowserView):
-    is_extended_schema = False # True if schema is generated for an api call and not for the usual form view
-    content_types_without_schema = ['Helptext', 'Button Handler']
+    is_extended_schema = False  # True if schema is generated for an api call and not for the usual form view
+    content_types_without_schema = ["Helptext", "Button Handler"]
 
     def __init__(self, context, request):
         super().__init__(context, request)
@@ -37,40 +37,50 @@ class JsonSchemaView(BrowserView):
         return self.jsonschema
 
     def set_json_base_schema(self):
-        self.jsonschema = {
-            'type': 'object'
-        }
-        self.jsonschema['properties'] = {}
-        self.jsonschema['required'] = []
-        self.jsonschema['dependentRequired'] = {}
-        self.jsonschema['allOf'] = []
+        self.jsonschema = {"type": "object"}
+        self.jsonschema["properties"] = {}
+        self.jsonschema["required"] = []
+        self.jsonschema["dependentRequired"] = {}
+        self.jsonschema["allOf"] = []
         self.ids = set()
 
     def add_child_to_schema(self, child_object, schema):
         """
         schema needs the keys 'properties' and 'required'
         """
-        if child_object.portal_type in self.content_types_without_schema or not check_show_condition_in_request(self.request, child_object.show_condition, child_object.negate_condition):
+        if (
+            child_object.portal_type in self.content_types_without_schema
+            or not check_show_condition_in_request(
+                self.request, child_object.show_condition, child_object.negate_condition
+            )
+        ):
             return
         child_id = self.create_and_check_id(child_object)
 
         # add children to the schema
-        if child_object.portal_type == 'Fieldset':
+        if child_object.portal_type == "Fieldset":
             schema = self.modify_schema_for_fieldset(schema, child_object)
-        elif 'properties' in schema:
-            schema['properties'][child_id] = self.get_schema_for_child(child_object)
+        elif "properties" in schema:
+            schema["properties"][child_id] = self.get_schema_for_child(child_object)
         else:
-            print("Error in JsonSchemaView: could not add child to schema, no 'properties' found in schema")
+            print(
+                "Error in JsonSchemaView: could not add child to schema, no 'properties' found in schema"
+            )
             return
 
         # mark children as required
-        if child_object.portal_type in possibly_required_types and child_object.required_choice == 'required':
+        if (
+            child_object.portal_type in possibly_required_types
+            and child_object.required_choice == "required"
+        ):
             if check_for_dependencies(child_object):
                 schema = self.add_dependent_required(schema, child_object, child_id)
-            elif 'required' in schema:
-                schema['required'].append(child_id)
+            elif "required" in schema:
+                schema["required"].append(child_id)
             else:
-                print("Error in JsonSchemaView: could not add requirements of child to schema, no 'required' found in schema")
+                print(
+                    "Error in JsonSchemaView: could not add requirements of child to schema, no 'required' found in schema"
+                )
                 return
 
     def modify_schema_for_fieldset(self, schema, fieldset):
@@ -83,60 +93,62 @@ class JsonSchemaView(BrowserView):
     def get_schema_for_child(self, child):
         type = child.portal_type
 
-        if type == 'Field':
+        if type == "Field":
             return self.get_schema_for_field(child)
-        elif type == 'SelectionField':
+        elif type == "SelectionField":
             return self.get_schema_for_selectionfield(child)
-        elif type == 'UploadField':
+        elif type == "UploadField":
             return self.get_schema_for_uploadfield(child)
-        elif type == 'Reference':
+        elif type == "Reference":
             return self.get_schema_for_reference(child)
-        elif type == 'Array':
+        elif type == "Array":
             return self.get_schema_for_array(child)
-        elif type == 'Complex':
+        elif type == "Complex":
             return self.get_schema_for_object(child)
         return {}
 
     def get_schema_for_field(self, field):
         field_schema = self.create_base_schema__field({}, field)
         answer_type = field.answer_type
-        if answer_type in ['text', 'textarea', 'password']:
-            field_schema['type'] = 'string'
+        if answer_type in ["text", "textarea", "password"]:
+            field_schema["type"] = "string"
             if field.minimum:
-                field_schema['minLength'] = field.minimum
+                field_schema["minLength"] = field.minimum
             if field.maximum:
-                field_schema['maxLength'] = field.maximum
-        elif answer_type == 'tel':
-            field_schema['type'] = 'string'
-            field_schema['pattern'] = '^\+?(\d{1,3})?[-.\s]?(\(?\d{1,4}\)?)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$'
+                field_schema["maxLength"] = field.maximum
+        elif answer_type == "tel":
+            field_schema["type"] = "string"
+            field_schema["pattern"] = (
+                "^\+?(\d{1,3})?[-.\s]?(\(?\d{1,4}\)?)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
+            )
             # or simply '^[\d+\-()\s]{1,25}$' (max 25 symbols, +, -, (, ), numbers and space are allowed
-        elif answer_type == 'url':
-            field_schema['type'] = 'string'
-            field_schema['format'] = 'hostname'
-        elif answer_type == 'email':
-            field_schema['type'] = 'string'
-            field_schema['format'] = 'email'
-        elif answer_type == 'date':
-            field_schema['type'] = 'string'
-            field_schema['format'] = 'date'
-        elif answer_type == 'datetime-local':
-            field_schema['type'] = 'string'
-            field_schema['format'] = 'date-time'
-        elif answer_type == 'time':
-            field_schema['type'] = 'string'
-            field_schema['format'] = 'time'
-        elif answer_type == 'number':
-            field_schema['type'] = 'number'
-        elif answer_type == 'integer':
-            field_schema['type'] = 'integer'
-        elif answer_type == 'boolean':
-            field_schema['type'] = 'boolean'
+        elif answer_type == "url":
+            field_schema["type"] = "string"
+            field_schema["format"] = "hostname"
+        elif answer_type == "email":
+            field_schema["type"] = "string"
+            field_schema["format"] = "email"
+        elif answer_type == "date":
+            field_schema["type"] = "string"
+            field_schema["format"] = "date"
+        elif answer_type == "datetime-local":
+            field_schema["type"] = "string"
+            field_schema["format"] = "date-time"
+        elif answer_type == "time":
+            field_schema["type"] = "string"
+            field_schema["format"] = "time"
+        elif answer_type == "number":
+            field_schema["type"] = "number"
+        elif answer_type == "integer":
+            field_schema["type"] = "integer"
+        elif answer_type == "boolean":
+            field_schema["type"] = "boolean"
 
-        if answer_type in ['number', 'integer']:
+        if answer_type in ["number", "integer"]:
             if field.minimum:
-                field_schema['minimum'] = field.minimum
+                field_schema["minimum"] = field.minimum
             if field.maximum:
-                field_schema['maximum'] = field.maximum
+                field_schema["maximum"] = field.maximum
         return field_schema
 
     def get_schema_for_selectionfield(self, selectionfield):
@@ -159,15 +171,12 @@ class JsonSchemaView(BrowserView):
                     local_options_list = vals
                 options_list.extend(local_options_list)
 
-        if answer_type == 'radio' or answer_type == 'select':
-            selectionfield_schema['type'] = 'string'
-            selectionfield_schema['enum'] = options_list
-        elif answer_type == 'checkbox' or answer_type == 'selectmultiple':
-            selectionfield_schema['type'] = 'array'
-            selectionfield_schema['items'] = {
-                'enum': options_list,
-                'type': 'string'
-            }
+        if answer_type == "radio" or answer_type == "select":
+            selectionfield_schema["type"] = "string"
+            selectionfield_schema["enum"] = options_list
+        elif answer_type == "checkbox" or answer_type == "selectmultiple":
+            selectionfield_schema["type"] = "array"
+            selectionfield_schema["items"] = {"enum": options_list, "type": "string"}
         return selectionfield_schema
 
     def get_schema_for_uploadfield(self, uploadfield):
@@ -178,20 +187,20 @@ class JsonSchemaView(BrowserView):
 
         if uploadfield.max_number_of_files:
             if uploadfield.max_number_of_files == 1:
-                uploadfield_schema['type'] = 'string'
-                uploadfield_schema['format'] = 'uri'
+                uploadfield_schema["type"] = "string"
+                uploadfield_schema["format"] = "uri"
             else:
-                uploadfield_schema['type'] = 'array'
-                uploadfield_schema['items'] = {
-                    'type': 'string',
-                    'format': 'uri',
+                uploadfield_schema["type"] = "array"
+                uploadfield_schema["items"] = {
+                    "type": "string",
+                    "format": "uri",
                 }
-                uploadfield_schema['maxItems'] = uploadfield.max_number_of_files
+                uploadfield_schema["maxItems"] = uploadfield.max_number_of_files
         else:
-            uploadfield_schema['type'] = 'array'
-            uploadfield_schema['items'] = {
-                'type': 'string',
-                'format': 'uri',
+            uploadfield_schema["type"] = "array"
+            uploadfield_schema["items"] = {
+                "type": "string",
+                "format": "uri",
             }
 
         return uploadfield_schema
@@ -202,28 +211,27 @@ class JsonSchemaView(BrowserView):
             if obj:
                 obj_schema = self.get_schema_for_child(obj)
                 return obj_schema
-        except: # referenced object got deleted, ignore
+        except:  # referenced object got deleted, ignore
             return {}
 
-
     def get_schema_for_array(self, array):
-        array_schema = self.add_title_and_description({'type': 'array'}, array)
+        array_schema = self.add_title_and_description({"type": "array"}, array)
         array_schema = add_interninformation(array_schema, array)
-        if array.required_choice == 'required':
-            array_schema['minItems'] = 1
-        array_schema['items'] = self.get_schema_for_object(array)
+        if array.required_choice == "required":
+            array_schema["minItems"] = 1
+        array_schema["items"] = self.get_schema_for_object(array)
         return array_schema
 
     def get_schema_for_object(self, object):
         complex_schema = {}
-        complex_schema['type'] = 'object'
-        if object.portal_type != 'Array':
+        complex_schema["type"] = "object"
+        if object.portal_type != "Array":
             complex_schema = self.add_title_and_description(complex_schema, object)
             complex_schema = add_interninformation(complex_schema, object)
-        complex_schema['properties'] = {}
-        complex_schema['required'] = []
-        complex_schema['dependentRequired'] = {}
-        complex_schema['allOf'] = []
+        complex_schema["properties"] = {}
+        complex_schema["required"] = []
+        complex_schema["dependentRequired"] = {}
+        complex_schema["allOf"] = []
 
         children = object.getFolderContents()
         for child in children:
@@ -236,8 +244,8 @@ class JsonSchemaView(BrowserView):
         dependencies = child_object.dependencies
 
         # copy 'allOf' and 'dependentRequired' to check that at least one of them changed
-        schema_allof_copy = copy.deepcopy(schema['allOf'])
-        schema_dependentrequired_copy = copy.deepcopy(schema['dependentRequired'])
+        schema_allof_copy = copy.deepcopy(schema["allOf"])
+        schema_dependentrequired_copy = copy.deepcopy(schema["dependentRequired"])
 
         for dep in dependencies:
             try:
@@ -251,31 +259,32 @@ class JsonSchemaView(BrowserView):
                                 create_id(selection_parent): {'const': get_title(dep, self.request)}
                             }
                         },
-                        'then': {
-                            'required': [child_id]
-                        }
+                        "then": {"required": [child_id]},
                     }
 
                     if self.is_extended_schema:
-                        if_then['else'] = create_else_statement(child_object)
+                        if_then["else"] = create_else_statement(child_object)
                     # if 'allOf' in schema:
                     #     schema['allOf'].append(if_then)
                     # else:
                     #     schema['allOf'] = [if_then]
-                    schema['allOf'].append(if_then)
+                    schema["allOf"].append(if_then)
                 else:
                     if dep_id in schema['dependentRequired']:
                         schema['dependentRequired'][dep_id].append(child_id)
                     else:
-                        schema['dependentRequired'][dep_id] = [child_id]
+                        schema["dependentRequired"][dep_id] = [child_id]
             except:
                 # dependency got deleted, plone error, ignore this dependency
                 continue
 
         # Check that at least one dependency wasn't deleted so that 'allOf' and/or 'dependentRequired' changed.
         # Otherwise add child_object to required-list of the schema, because it is required and not dependent required, if it has no valid dependencies anymore
-        if schema_allof_copy == schema['allOf'] and schema_dependentrequired_copy == schema['dependentRequired']:
-            schema['required'].append(child_id)
+        if (
+            schema_allof_copy == schema["allOf"]
+            and schema_dependentrequired_copy == schema["dependentRequired"]
+        ):
+            schema["required"].append(child_id)
         return schema
 
     # left over of duplicate check. method not deleted in case of other stuff with ids in the future
@@ -288,6 +297,7 @@ class JsonSchemaView(BrowserView):
     """
     create base schema for a field, selectionfield or an uploadfield
     """
+
     def create_base_schema__field(self, schema, child):
         base_schema = self.add_title_and_description(schema, child)
         # base_schema = add_userhelptext(base_schema, child)
@@ -295,13 +305,12 @@ class JsonSchemaView(BrowserView):
         return base_schema
 
     def add_title_and_description(self, schema, child):
-        schema['title'] = get_title(child, self.request)
+        schema["title"] = get_title(child, self.request)
         description = get_description(child, self.request)
         if description:
-            schema['description'] = description
+            schema["description"] = description
 
         return schema
-
 
 
 def check_for_dependencies(child_object):
@@ -310,16 +319,14 @@ def check_for_dependencies(child_object):
     else:
         return False
 
+
 def add_interninformation(schema, child):
-    if hasattr(child, 'intern_information') and child.intern_information:
-        schema['comment'] = child.intern_information
+    if hasattr(child, "intern_information") and child.intern_information:
+        schema["comment"] = child.intern_information
     return schema
 
+
 def create_else_statement(child_object):
-    if child_object.portal_type == 'Field':
+    if child_object.portal_type == "Field":
         if child_object.answer_type in string_type_fields:
-            return {
-                'properties': {
-                    create_id(child_object): {'maxLength': 0}
-                }
-            }
+            return {"properties": {create_id(child_object): {"maxLength": 0}}}
