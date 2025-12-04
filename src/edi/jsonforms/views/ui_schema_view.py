fc7356fdc3b74d4243cd5f2b0c480f51c4ab1690
Julian Pollinger <julian@pollinger.dev>
2025-12-04 17:04:55 +0100

    feat: rita dependent options

--- src/edi/jsonforms/views/ui_schema_view.py
+++ src/edi/jsonforms/views/ui_schema_view.py
@@ -11,6 +11,10 @@ from edi.jsonforms.views.common import *
 from edi.jsonforms.views.showOn_properties import create_showon_properties
 from edi.jsonforms.content.option_list import get_keys_and_values_for_options_list
 
+from plone.base.utils import safe_hasattr
+
+from edi.jsonforms.views.json_schema_view import get_option_name
+
 
 class UiSchemaView(BrowserView):
     tools_on = False
@@ -189,6 +193,16 @@ class UiSchemaView(BrowserView):
                     selectionfield_schema["options"]["enumTitles"] = dict(
                         zip(keys, vals)
                     )
+
+        option_filters = {}
+        options = selectionfield.getFolderContents()
+        for option in options:
+            o = option.getObject()
+            if safe_hasattr(o, "ritarules"):
+                formatted_rita_rule = json.loads(o.ritarules)
+                option_filters[get_option_name(o)] = formatted_rita_rule
+        if option_filters:
+            selectionfield_schema["options"]["optionFilters"] = option_filters
 
         if selectionfield_schema["options"] == {}:
             del selectionfield_schema["options"]
