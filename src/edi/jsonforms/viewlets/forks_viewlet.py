cc9b9107e9c6c7d0199e8d9f623cc7cc9d6cd4d4
Nina Mücke <nina.muecke@educorvi.de>
2026-03-04 12:14:26 +0100

    feat: viewlet to show available show_conditions (forks)

---
+++ src/edi/jsonforms/viewlets/forks_viewlet.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from plone.app.layout.viewlets import ViewletBase
+from plone.base.utils import safe_hasattr
+
+from edi.jsonforms.content.form import IForm
+from edi.jsonforms.content.wizard import IWizard
+from edi.jsonforms.content.common import IFormElement
+
+
+class ForksViewlet(ViewletBase):
+    def render(self):
+        url = self.request.get("URL", "")
+        if url.endswith("form-tools-view") or url.endswith("wizard-tools-view"):
+            return super().render()
+        return ""
+
+    def get_available_forks(self) -> list[str]:
+        """
+        gets all show_conditions of the current object and its children recursively
+        deletes duplicates and returns the list
+        """
+        forks = self._get_available_forks(self.context)
+        # delete duplicates
+        forks = list(set(forks))
+        return forks
+
+    def _get_available_forks(self, obj: IForm | IWizard | IFormElement) -> list[str]:
+        """
+        recursively traverses all children of obj and gets all show_conditions in a list
+        duplicates are not removed
+        """
+        forks = []
+        for child in obj.restrictedTraverse("@@contentlisting")():
+            if safe_hasattr(child, "show_condition") and child.show_condition:
+                forks.append(child.show_condition)
+            if safe_hasattr(child, "is_folderish") and child.is_folderish:
+                forks.extend(self._get_available_forks(child.getObject()))
+        return forks
