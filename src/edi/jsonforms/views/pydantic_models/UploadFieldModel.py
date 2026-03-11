ea1fc51eca0d62faa9271af843dfa2f611afc30d
Nina Mücke <nina.muecke@educorvi.de>
2026-03-11 16:37:24 +0100

    refactor(json-schema): uploadfield

--- src/edi/jsonforms/views/pydantic_models/UploadFieldModel.py
+++ src/edi/jsonforms/views/pydantic_models/UploadFieldModel.py
@@ -1,6 +1,7 @@
 import logging
 from typing import Optional, List
 from ZPublisher.HTTPRequest import WSGIRequest, HTTPRequest
+from pydantic import BaseModel
 
 from edi.jsonforms.content.upload_field import IUploadField
 
@@ -11,11 +12,20 @@ from edi.jsonforms.views.pydantic_models.BaseFormElementModel import (
 logger = logging.getLogger(__name__)
 
 
+class Upload(BaseModel):
+    type: str = "string"
+    format: Optional[str] = "uri"
+
+    def get_json_schema(self):
+        return self.model_dump(exclude_none=True)
+
+
 class UploadFieldModel(BaseFormElementModel):
     minItems: Optional[int] = None
     maxItems: Optional[int] = None
     format: Optional[str] = None
-    items: Optional[dict] = None
+    items: Optional[Upload] = None
+    data: Optional[Upload] = None
     # maxSize: Optional[int] = None
     # allowedTypes: Optional[List[str]] = None
 
@@ -28,26 +38,26 @@ class UploadFieldModel(BaseFormElementModel):
         super().__init__(form_element, parent_model, request)
         self.minItems = form_element.min_number_of_files
 
+        upload = Upload()
         if form_element.max_number_of_files:
             if form_element.max_number_of_files == 1:
-                self.type = "string"
-                self.format = "uri"
+                self.data = upload
             else:
                 self.type = "array"
-                self.items = {
-                    "type": "string",
-                    "format": "uri",
-                }
+                self.items = upload
                 self.maxItems = form_element.max_number_of_files
         else:
             self.type = "array"
-            self.items = {
-                "type": "string",
-                "format": "uri",
-            }
+            self.items = upload
 
         # self.maxSize = form_element.max_file_size
         # self.allowedTypes = form_element.accepted_file_types
 
     def get_json_schema(self):
-        return super().get_json_schema()
+        excluded_fields = self.get_json_dump_exclude_list().union({"data", "items"})
+        json_schema = self.model_dump(exclude=excluded_fields, exclude_none=True)
+        if self.data:
+            json_schema.update(self.data.get_json_schema())
+        elif self.items:
+            json_schema["items"] = self.items.get_json_schema()
+        return json_schema
